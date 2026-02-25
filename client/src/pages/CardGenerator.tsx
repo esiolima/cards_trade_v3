55b457e2d0bb59f05478fb567679519e049f9aef
esiolima <132793812+esiolima@users.noreply.github.com>
2026-02-25 09:49:38 -0300

    Update CardGenerator.tsx

--- client/src/pages/CardGenerator.tsx
+++ client/src/pages/CardGenerator.tsx
@@ -263,7 +263,7 @@ export default function CardGenerator() {
 
         <div className={`mt-16 pt-8 border-t ${borderColor} text-center`}>
           <p className={`text-sm ${textSecondary}`}>
-            Desenvolvido por Esio Lima - Versão 2.2
+            Desenvolvido por Esio Lima - Versão 3.1
           </p>
         </div>
       </div>
